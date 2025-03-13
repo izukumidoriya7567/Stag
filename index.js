6cd99a40c9025b4b93525f951302f214be34ae37
arpit <arpitghr12@gmail.com>
2025-03-13 22:54:28 +0530

    Weasley

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express=require("express");
 const app=express();
+const port=process.env.PORT;
 app.get("/",(req,res)=>{
     res.send("Hello bro");
 })
 app.get("/west",(req,res)=>{
-    res.send("Lopez");
+    res.send(`Lopez:${port}`);
 })
 app.get("/chen",(req,res)=>{
     res.send("Tim Bradford");
